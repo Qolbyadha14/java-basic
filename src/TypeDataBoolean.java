88561a7405a52ecf35270ed9ccf1a998ba3c252e
qolby.adha <qolby.adha@kawanlamacorp.com>
2024-05-15 21:02:37 +0700

    course 4: type data boolean

---
+++ src/TypeDataBoolean.java
@@ -0,0 +1,9 @@
+public class TypeDataBoolean {
+    public static void main(String[] args) {
+        boolean b = true;
+        System.out.println(b);
+
+        b = false;
+        System.out.println(b);
+    }
+}
