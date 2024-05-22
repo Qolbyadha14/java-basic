13a23f307864b6c27ce5ce8b9edc2a2a22f2f522
qolby.adha <qolby.adha@kawanlamacorp.com>
2024-05-22 09:09:45 +0700

    course 28: Comment

---
+++ src/Comment.java
@@ -0,0 +1,19 @@
+public class Comment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(sum(3, 5));
+    }
+    /**
+     * Comment multi line
+     * Calculates the sum of two integers.
+     *
+     * @param  a  the first integer to be added
+     * @param  b  the second integer to be added
+     * @return    the sum of the two integers
+     */
+    static int sum(int a, int b) {
+        // comment single line
+        return a + b;
+    }
+}
