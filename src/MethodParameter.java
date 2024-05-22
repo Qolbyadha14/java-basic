3f6bc58d56dbf0299f89f294fe93b50227630323
qolby.adha <qolby.adha@kawanlamacorp.com>
2024-05-22 08:54:06 +0700

    course 22: Method Parameter

---
+++ src/MethodParameter.java
@@ -0,0 +1,12 @@
+public class MethodParameter {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        myMethod(5);
+    }
+
+    public static void myMethod(int x) {
+        System.out.println(x);
+    }
+}
