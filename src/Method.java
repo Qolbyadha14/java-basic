294f0e8ea70a54eed4c994ccc6eb2471f3a2b13e
qolby.adha <qolby.adha@kawanlamacorp.com>
2024-05-22 08:53:23 +0700

    course 21: Method

---
+++ src/Method.java
@@ -0,0 +1,12 @@
+public class Method {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        myMethod();
+    }
+
+    public static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+}
