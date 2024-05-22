ddc232988f36263db1ec1eb2c9c9ffbf1e54e584
qolby.adha <qolby.adha@kawanlamacorp.com>
2024-05-22 08:30:00 +0700

    course 9: math operators

---
+++ src/MathematicalOperators.java
@@ -0,0 +1,22 @@
+public class MathematicalOperators {
+    public static void main(String[] args) {
+        float a = 10;
+        float b = 20;
+        float c = 30;
+        float d = 40;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("d % a = " + (d % a));
+
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("c++ = " + (c++));
+        System.out.println("d-- = " + (d--));
+    }
+}
