0abdefbf8538f330c09ab4a0d665d106b70da298
qolby.adha <qolby.adha@kawanlamacorp.com>
2024-05-22 08:46:05 +0700

    course 15: Ternary Operator

---
+++ src/TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        var nilai = 80;
+        var ucapan = (nilai >= 75) ? "Lulus" : "Tidak Lulus";
+        System.out.println(ucapan);
+    }
+}
